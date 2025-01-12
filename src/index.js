a3bb855736ba10ce46dd5ebc04a48ff57e5e5d4b
Kruthik S <gokruthik2003@gmail.com>
2025-01-12 16:14:44 +0530

    Connection to DB, added prettier file for consistency

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("first");
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+import "dotenv/config";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
